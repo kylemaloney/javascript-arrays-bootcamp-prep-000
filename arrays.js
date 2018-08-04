1f8948927f0d6bce4d9072a535ba5e9a3f768da7
kylemaloney17@gmail.com <kylemaloney17@gmail.com>
2018-08-04 17:34:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function destructivelyRemoveElementFromBeginningOfArray (array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.unshift();
+  array.slice();
   return array;
 }
 
