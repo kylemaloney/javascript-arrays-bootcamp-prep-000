9e2daacff7fb5e094c35b49870c3c9d6f33c52c8
kylemaloney17@gmail.com <kylemaloney17@gmail.com>
2018-08-04 17:08:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,24 @@ var chocolateBars = [
   'hundred grand',
   'kitkat',
   'skittles'];
+
+function addElementToBeginningOfArray(x, add){
+  var y = [add, ...x];
+  return y;
+}
+
+function destructivelyAddElementToBeginningOfArray (x, add){
+  x = [add, ...x];
+  return x; 
+}
+
+function addElementToEndOfArray(x, add){
+  var y = [...x, add];
+  return y;
+}
+
+function destructivelyAddElementToEndOfArray(x, add){
+  x = [...x, add];
+  return x; 
+}
+
